85f02d7f5291501785aa53e995366fa674b8ea4a
weili <liwei72439936@gmail.com>
2019-05-18 16:36:38 +0800

    React Data access/Web, Selenium test

---
+++ ch04-Test/learning-spring-boot/src/main/java/com/greglturnquist/learningspringboot/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.greglturnquist.learningspringboot;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface ImageRepository extends ReactiveCrudRepository<Image, String> {
+    Mono<Image> findByName(String name);
+}
---
+++ ch04-Test/learning-spring-boot/src/test/java/com/greglturnquist/learningspringboot/ImageTest.java
@@ -0,0 +1,14 @@
+package com.greglturnquist.learningspringboot;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ImageTest {
+    @Test
+    public void imagesManagedByLombokShouldWork() {
+        Image image = new Image("id", "file-name.jpg");
+        assertThat(image.getId()).isEqualTo("id");
+        assertThat(image.getName()).isEqualTo("file-name.jpg");
+    }
+}
