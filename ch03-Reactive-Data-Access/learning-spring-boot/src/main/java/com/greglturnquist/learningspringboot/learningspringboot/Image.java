118b27742ed64b6c5f989f6494305c4d71a41af2
weili <liwei72439936@gmail.com>
2019-05-18 13:01:09 +0800

    Reacteive-Data-Access

---
+++ ch03-Reactive-Data-Access/learning-spring-boot/src/main/java/com/greglturnquist/learningspringboot/learningspringboot/Image.java
@@ -0,0 +1,12 @@
+package com.greglturnquist.learningspringboot.learningspringboot;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Image {
+    @Id private final String id;
+    private final String name;
+}
