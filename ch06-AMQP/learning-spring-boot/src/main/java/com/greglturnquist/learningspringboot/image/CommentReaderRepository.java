43213a69d5401e542d787317950619d4d89aee9f
Wei Li <liwei72439936@gmail.com>
2019-05-20 18:36:26 +0800

    start AMQP

---
+++ ch06-AMQP/learning-spring-boot/src/main/java/com/greglturnquist/learningspringboot/image/CommentReaderRepository.java
@@ -0,0 +1,9 @@
+package com.greglturnquist.learningspringboot.image;
+
+import org.springframework.data.repository.Repository;
+import reactor.core.publisher.Flux;
+
+public interface CommentReaderRepository extends Repository<Comment,String> {
+
+    Flux<Comment> findByImageId(String imageId);
+}
